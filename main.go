ffbb736bbfd83908e9119db1f249f508501b4085
Mike Harvey <harvey.miche@gmail.com>
2023-06-29 23:06:52 +1200

    Add api file

--- main.go
+++ main.go
@@ -1,26 +1,6 @@
 package main
 
-import (
-	"encoding/json"
-	"net/http"
-)
-
-type Todo struct {
-	Id        int    `json:"id"`
-	Task      string `json:"task"`
-	Completed bool   `json:"completed"`
-}
-
 func main() {
-	http.HandleFunc("/todos", func(w http.ResponseWriter, r *http.Request) {
-		todos := Todo{
-			Id:        1,
-			Task:      "First todo item",
-			Completed: false,
-		}
-
-		json.NewEncoder(w).Encode(todos)
-	})
-
-	http.ListenAndServe(":4000", nil)
+	server := newAPIServer(":4000")
+	server.Run()
 }
